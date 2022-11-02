24a2575e9bc9fe8088e9019bfccef6ca7d255a53
Vaibhav2998 <vaibhavvishal1244@gmail.com>
2022-11-02 08:57:07 +0530

    day one first commint C++

---
+++ Lecture2_Write Your First Program in C++/first.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    cout << "Namaste Dunya" << endl;
+
+    int a = 123;
+    cout << a << endl;
+
+    char ch = 'a';
+    cout << ch << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.23;
+    cout << d << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a is: " << size << endl;
+
+    */
+
+    int a = 'a';
+
+    cout << a << endl;
+
+    unsigned int b = -112;
+
+    cout << b << endl;
+}
