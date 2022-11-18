c5954f01d22673d8706bff755440963b6160d2f0
Vaibhav2998 <vaibhavvishal1244@gmail.com>
2022-11-18 07:29:25 +0530

    second commit

--- Sorting Algorithm/Bubblesort.cpp
+++ Sorting Algorithm/Bubblesort.cpp
@@ -5,14 +5,20 @@ using namespace std;
 void bubbleSort(int arr[], int size)
 {
 
+    
     for (int i = 0; i < size; i++)
     {
+        bool isSwap = false;
         for (int j = 0; j < size - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                isSwap = true;
             }
+        }
+        if(isSwap == false){
+            break;
         }
     }
 }
---
+++ Sorting Algorithm/insertionsort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        for (; j >= 0; j--)
+        {
+            if (temp < arr[j])
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int arr[6] = {6, 22, 2, 8, 4, 10};
+    cout << "Before Insertion Sort: " << endl;
+    printArray(arr, 6);
+    cout << "After Insertion Sort: " << endl;
+    insertionSort(arr, 6);
+    printArray(arr, 6);
+}
