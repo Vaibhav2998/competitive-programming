0c53079b99c2394deb0ddf3fff9a7cafe7bfd14c
Vaibhav2998 <vaibhavvishal1244@gmail.com>
2022-12-02 07:20:22 +0530

    commiting

--- Phase2 Data Structure/Linked List/intro.cpp
+++ Phase2 Data Structure/Linked List/intro.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
+class Node {
+
+public:
+  int data;
+  Node* next;
+
+  Node(int data) {
+    this -> data = data;
+    this -> next = NULL;
+  }
+};
+
 int main() {
 
-  cout << "LINKED LIST" <<endl;
+  Node* node1 = new Node(10);
+  cout << node1 -> data << endl;
+  cout << node1 -> next << endl;
 }
