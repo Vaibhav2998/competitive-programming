07d178dc2a9c4f958c163bff46cd7f59129cfedf
Vaibhav2998 <vaibhavvishal1244@gmail.com>
2022-12-06 08:03:12 +0530

    first commit

---
+++ Started Again/Phase 1/02_Bit_manipulation(luvB)/sumnumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() { 
+  int n;
+  cin >> n;
+  int summ = 0;
+  for(int i=1; i <= n; i++){
+    summ += i;
+  }
+
+  cout << "Sum is: " << summ << endl;
+}
